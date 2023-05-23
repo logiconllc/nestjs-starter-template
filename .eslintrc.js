2e709000070741ed99203ae433a73a5c7c07a519
Adil Sarwar <adilsarwar@logicon.com.pk>
2023-05-24 00:49:53 +0500

    Updated eslintr

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,21 @@
 module.exports = {
-    root: true,
-    plugins: ['@typescript-eslint', 'import', 'prettier'],
-    extends: [
-      'airbnb-typescript/base',
-      'prettier',
-      'plugin:@typescript-eslint/recommended',
-      'plugin:import/typescript',
-    ],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-      project: './tsconfig.eslint.json',
-    },
-    "rules": {
-      // Note: you must disable the base rule as it can report incorrect errors
-      "no-unused-vars": "off",
-      "@typescript-eslint/no-unused-vars": "warn",
-      "no-shadow": "off",
-      "@typescript-eslint/no-shadow": "error"
-    }
-  };
+  root: true,
+  plugins: ['@typescript-eslint', 'import'],
+  extends: [
+    'airbnb-typescript/base',
+    'prettier',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/typescript',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.eslint.json',
+  },
+  "rules": {
+    // Note: you must disable the base rule as it can report incorrect errors
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "warn",
+    "no-shadow": "off",
+    "@typescript-eslint/no-shadow": "error"
+  }
+};
